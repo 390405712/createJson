763f9d9b0b5f9d0bddc82be1e6b74dd687ca0630
wulihua <11111111>
2019-01-31 15:28:03 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+/*---滚动条默认显示样式--*/
+::-webkit-scrollbar-thumb {
+  background-color: #E4E3E4;
+  height: 50px;
+  outline-offset: -2px;
+  outline: 2px solid #fff;
+  -webkit-border-radius: 8px;
+  /*border: 2px solid #fff;*/
+}
+
+/*---鼠标点击滚动条显示样式--*/
+::-webkit-scrollbar-thumb:hover {
+  background-color: #409EFF;
+  height: 50px;
+  -webkit-border-radius: 8px;
+}
+
+/*---滚动条大小--*/
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+/*---滚动框背景样式--*/
+::-webkit-scrollbar-track-piece {
+  background-color: #fff;
+  -webkit-border-radius: 0;
+}
+
+body {
+  background-color: #F1F5F8;
+}
+
+.el-select {
+  width: 100% !important;
+}
+
+#re {
+  border: 1px solid #e2e2e2;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+.el-card {
+  margin-bottom: 15px;
+}
+
+.el-aside {
+  padding: 20px 0 0 20px;
+}
+
+#biaoti {
+  font-size: 16pt;
+  text-align: center;
+  font-weight: bold;
+  font-family: serif;
+  color: #606266;
+}
+
+.el-main {
+  padding: 20px 20px 0px 15px !important;
+}
+
+.el-button-group .el-button--warning:first-child {
+  border-right-color: #f5dab1;
+}
+
+.el-button-group {
+  margin-bottom: 10px;
+}
+
+.cell {
+  text-align: center;
+}
+
+.el-select {
+  width: 150px !important;
+}
+
+.el-input__prefix, .el-input__suffix {
+  line-height: 38px;
+  font-size: 16pt;
+}
+
+i {
+  font-style: inherit;
+}
+
+.el-table_1_column_2 {
+  width: 20% !important;
+}
+
+.demo-table-expand {
+  font-size: 0;
+}
+
+.demo-table-expand label {
+  width: 100px;
+  color: #99a9bf;
+}
+
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
+
+tr td:last-child {
+  background-color: #fff;
+}
+
+.el-aside .el-card__body {
+  height: calc(100% - 40px);
+}
+
+#content .is-always-shadow {
+  margin-bottom: 0 !important;
+  height: calc(100% - 10px) !important;
+}
+.div-tree{
+  border: 1px solid #dcdfe6!important;
+  height: 440px;
+  border-radius: 4px;
+  padding: 10px;
+}
+.el-tree{
+  height: 380px;
+  overflow: auto;
+  margin-top: 10px;
+}
+.el-tabs{
+  /*border: 1px solid #dcdfe6!important;*/
+  height: 462px;
+}
+.el-tabs__content{
+  height: 462px;
+}
+.pro{
+  height: calc(100% - 10px);
+  margin: 0;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,449 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <el-card shadow="always" id="biaoti">生成json[加强美化版]3.3</el-card>
+      </el-header>
+      <el-container id="content">
+        <el-aside style="width:180px;">
+          <transition name="el-zoom-in-top">
+            <el-card shadow="always" class="pro">
+              <div style="height: 100%;">
+                <el-steps direction="vertical" :active="3">
+                  <el-step title="步骤 1" description="填写循环次数"></el-step>
+                  <el-step title="步骤 2" description="添加对象"></el-step>
+                  <el-step title="步骤 3" description="点击生成"></el-step>
+                </el-steps>
+              </div>
+            </el-card>
+          </transition>
+        </el-aside>
+        <el-main>
+          <el-card shadow="hover">
+            <template>
+              <div>循环次数：{{xunhuan}}次</div>
+              <div class="silder-width">
+                <el-slider :max=200000 :min=1 :disabled="loading" v-model="xunhuan" show-input></el-slider>
+              </div>
+            </template>
+            <template>
+              <div>值类型为array[多个]数组长度：{{stringArr}}</div>
+              <div class="silder-width">
+                <el-slider :max=200000 :min=1 v-model="stringArr" :disabled="loading" show-input>
+                </el-slider>
+              </div>
+            </template>
+          </el-card>
+          <el-card class="box-card" shadow="hover">
+            <el-button-group>
+              <el-button id="addd" size="medium" type="primary" plain :disabled="loading" @click="isTrue('add')">添加属性</el-button>
+              <el-button size="medium" type="danger" :disabled="loading" plain @click="clearAll()">清除全部</el-button>
+              <el-popover placement="right-end" width="450" trigger="click">
+                <div>
+                  <p style="font-size: 16px;margin:0;">预览效果只循环2次,array[多个]长度为3</p>
+                  <el-input type="textarea" :autosize="{ minRows: 2,maxRows: 15}" style="font-size: 20px;font-family: -webkit-body;" v-model="result">
+                  </el-input>
+                </div>
+                <el-button size="medium" type="warning" plain slot="reference" :disabled="loading" @click="YuLan()">预览效果</el-button>
+              </el-popover>
+            </el-button-group>
+            <el-table v-loading="loading" element-loading-text="生成中" element-loading-background="rgba(241, 245, 248, 0.8)" stripe border :data="info" style="width: 100%">
+              <el-table-column label="ID" type="index" fixed width="50">
+              </el-table-column>
+              <el-table-column type="expand">
+                <template slot-scope="props">
+                  <el-form label-position="left" inline class="demo-table-expand">
+                    <el-form-item label="对象属性名：">
+                      <span>{{ props.row.valueName }}</span>
+                    </el-form-item>
+                    <el-form-item label="对象值类型：">
+                      <span>{{ props.row.type }}</span>
+                    </el-form-item>
+                    <el-form-item label="对象属性值：">
+                      <span>{{ props.row.value }}</span>
+                    </el-form-item>
+                    <el-form-item label="变量：">
+                      <span>{{ props.row.checked }}</span>
+                    </el-form-item>
+                  </el-form>
+                </template>
+              </el-table-column>
+              <el-table-column label="对象">
+                <el-table-column label="对象属性名"min-width="160":resizable="true">
+                  <template slot-scope="scope">
+                    <el-input :maxlength=20 type="text" v-model="scope.row.valueName">
+                      <i slot="suffix">:&nbsp;</i>
+                    </el-input>
+                  </template>
+                </el-table-column>
+                <el-table-column
+                  min-width="480"
+                  label="值类型 / 对象属性值 / 变量方式"
+                  :resizable="true">
+                  <template slot-scope="scope">
+                    <el-input :maxlength=20 type="text" v-model="scope.row.value" class="input-with-select">
+                      <el-select v-model="scope.row.type" slot="prepend" @change="selectChange1" placeholder="请选择">
+                        <el-option v-for="item in option1" :key="item.value" :label="item.value" :value="item.value">
+                        </el-option>
+                      </el-select>
+                      <el-select v-model="scope.row.checked" slot="append" @change="selectChange2(scope.row.checked,scope.$index)" placeholder="请选择">
+                        <el-option v-for="item in option2" :key="item.value" :label="item.value" :value="item.value">
+                        </el-option>
+                      </el-select>
+                    </el-input>
+                  </template>
+                </el-table-column>
+              </el-table-column>
+              <el-table-column width="100" label="操作" fixed="right">
+                <template slot-scope="scope">
+                  <el-button v-show="info.length>1" size="medium" plain type="danger" :disabled="loading" @click="del(scope.$index)">删除
+                  </el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-card>
+          <el-card shadow="hover" style="margin-bottom:8px">
+            <el-button id="btn" style="width: 100%" type="success" plain @click="isTrue('result')" :disabled="loading">生成</el-button>
+          </el-card>
+        </el-main>
+      </el-container>
+    </el-container>
+    <el-dialog
+      title="生成结果"
+      :visible.sync="dialogVisible">
+      <div>
+        <el-tabs tab-position="left" @tab-click="handleClick" v-model="activeName">
+          <el-tab-pane label="字符串形式" name="0">
+            <el-button style="position: absolute;right: 0;" size="mini" type="success" @click="CopyUrl()">复 制</el-button>
+            <el-input resize="none" id="copyObj" type="textarea" :autosize="{ minRows: 2,maxRows: 15}" style="font-size: 20px;font-family: -webkit-body;" v-model="result">
+            </el-input>
+          </el-tab-pane>
+          <el-tab-pane label="树形式" name="1">
+            <div class="div-tree">
+              <span>最多显示循环1000次的结果,渲染过多会导致画面掉帧</span>
+              <el-input size="mini" placeholder="输入关键字进行过滤" v-model="filterText">
+              </el-input>
+              <el-tree :data="resultJson" ref="tree" :filter-node-method="filterNode">
+              </el-tree>
+              <!--show-checkbox-->
+              <!--node-key="id"-->
+              <!--highlight-current-->
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: [
+        {valueName: '', value: '', type: 'string', checked: '不加变量'},
+      ],
+      xunhuan: 1,
+      stringArr: 1,
+      result: '[]',
+      option1: [{
+        value: 'number',
+      }, {
+        value: 'string',
+      }, {
+        value: 'array[单个]',
+      }, {
+        value: 'array[多个]',
+      }],
+      option2: [{
+        value: '不加变量',
+      }, {
+        value: '后面加',
+      }, {
+        value: '累加(值为数字)',
+      }],
+      value: '',
+      dialogVisible: false,
+      loading: false,
+      resultJson:[],
+      filterText: '',
+      activeName: '0',
+    }
+  },
+  watch:{
+    filterText(val) {
+      this.$refs.tree.filter(val);
+    },
+  },
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+    handleClick(tab, event) {
+      if(tab.index=='1'){
+        let _self = this;
+        // 树的数据处理
+        let arr =  JSON.parse(_self.result)
+        let newArr = [];
+        // let idNum = 0;
+        let arrLength = _self.xunhuan;
+        if(arrLength>1000){
+          arrLength = 1000
+        }
+        for(let i = 0;i<arrLength;i++){
+          // idNum++
+          newArr.push({
+            // id:idNum,
+            label:i+'{'+Object.keys(arr[i]).length+'}',
+            children:objToArray(arr[i])
+          })
+        }
+        function objToArray(array) {
+          let objToArray = []
+          for (let i in array) {
+            // idNum++
+            objToArray.push({
+              // id:idNum,
+              label:JSON.stringify(i)+':'+JSON.stringify(array[i])
+            });
+          }
+          return objToArray;
+        }
+        _self.resultJson = [{
+          label:'array['+arrLength+']',
+          children:newArr
+        }]
+      }
+    },
+    // getCheckedNodes() {
+    //   console.log(this.$refs.tree.getCheckedNodes());
+    //   let nodes = this.$refs.tree.getCheckedNodes();
+    //   let arr = [];
+    //   for(let i = 0;i<nodes.length;i++){
+    //     for(let j = 0;j<nodes[i].children.length;j++){
+    //       let obj='';
+    //       for(let k = 0;k<nodes[i].children[j].children.length;k++){
+    //         obj+=nodes[i].children[j].children[k].label
+    //       }
+    //       console.log(obj);
+    //       // arr.push({obj})
+    //     }
+    //   }
+    //   // JSON.stringify(arr);
+    //   // console.log(arr);
+    // },
+    resultFun() {
+      console.time('timer')
+      let _self = this;
+      let num = 0;
+      let str = '';
+      let xunhuan = _self.xunhuan;
+      let info = _self.info.length;
+      let stringArr = _self.stringArr;
+      for (let n = 0; n < xunhuan; n++) {
+        for (let i = 0; i < info; i++) {
+          let data = _self.info;
+          let key = data[i].valueName;
+          let val = data[i].value;
+          let type = data[i].type;
+          let checked = data[i].checked;
+          if (checked == '不加变量') {
+            if (type == 'number') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + (i == info - 1 ? '' : ',\n')
+            } else if (type == 'string') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + (i == info - 1 ? '"' : '",\n')
+            } else if (type == 'array[单个]') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + (i == info - 1 ? ']' : '],\n')
+            } else if (type == 'array[多个]') {
+              let strArr = '';
+              for (let m = 0; m < stringArr; m++) {
+                strArr += val + (m == stringArr - 1 ? '' : ',')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == info - 1 ? ']' : '],\n')
+            }
+          } else if (checked == '后面加') {
+            if (type == 'number') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + num + (i == info - 1 ? '' : ',\n')
+            } else if (type == 'string') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + num + (i == info - 1 ? '"' : '",\n')
+            } else if (type == 'array[单个]') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + num + (i == info - 1 ? ']' : '],\n')
+            } else if (type == 'array[多个]') {
+              let strArr = '';
+              for (let m = 0; m < stringArr; m++) {
+                strArr += '"' + val + m.toString() + (m == stringArr - 1 ? '"' : '",')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == info - 1 ? ']' : '],\n')
+            }
+          } else {
+            if (type == 'number') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + (i == info - 1 ? '' : ',\n')
+            } else if (type == 'string') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + (i == info - 1 ? '"' : '",\n')
+            } else if (type == 'array[单个]') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + (i == info - 1 ? ']' : '],\n')
+            } else if (type == 'array[多个]') {
+              let strArr = '';
+              val = parseInt(val);
+              for (let m = 0; m < stringArr; m++) {
+                strArr += '"' + (val + m) + (m == stringArr - 1 ? '"' : '",')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == info - 1 ? ']' : '],\n')
+            }
+          }
+        }
+        str = str + (n == xunhuan - 1 ? '\r\n  }\n' : '\r\n  },\r\n');
+        num++;
+      }
+      str = "[\r\n" + str + "]";
+      _self.result = str;
+      _self.loading = false;
+      _self.dialogVisible = true
+      _self.activeName='0',
+        console.timeEnd('timer')
+    },
+    isTrue(value) {
+      let _self = this;
+      for (let i = 0; i < _self.info.length; i++) {
+        if (_self.info[i].valueName == '' || _self.info[i].value == '') {
+          if (value == 'add') {
+            this.$message({
+              message: '填写完整再添加',
+              type: 'warning'
+            });
+            return
+          } else {
+            this.$message({
+              message: '填写完整再生成',
+              type: 'warning'
+            });
+            return
+          }
+        }
+      }
+      if (value == 'add') {
+        _self.info.push(
+          {valueName: '', value: '', type: 'string', checked: '不加变量'},
+        )
+      } else {
+        _self.loading = true;
+        window.localStorage.setItem('info', JSON.stringify(_self.info))
+        setTimeout(function () {
+          _self.resultFun();
+        }, 500)
+      }
+    },
+    del(index) {
+      let _self = this;
+      _self.info.splice(index, 1);
+    },
+    CopyUrl() {
+      let url = document.querySelector('#copyObj');
+      url.select();
+      document.execCommand("Copy");
+      url.blur()
+    },
+    clearAll() {
+      let _self = this;
+      _self.info = [
+        {valueName: '', value: '', type: 'string', checked: '不加变量'},
+      ],
+        window.localStorage.clear();
+    },
+    selectChange1(value) {
+      let _self = this;
+      if (value == 'array[多个]') {
+        this.$message('array[多个]的数组长度为' + _self.stringArr);
+      }
+    },
+    selectChange2(value, index) {
+      let _self = this;
+      if (_self.info[index].type == 'array[多个]') {
+        if (value != '不加变量') {
+          this.$message('array[多个]：出现的变量是在自身数组中');
+        }
+      }
+    },
+    YuLan() {
+      let _self = this;
+      let num = 0;
+      let str = '';
+      let yulanNum = 3;
+      for (let n = 0; n < 2; n++) {
+        for (let i = 0; i < _self.info.length; i++) {
+          let data = _self.info;
+          let key = data[i].valueName;
+          let val = data[i].value;
+          if (data[i].checked == '不加变量') {
+            if (data[i].type == 'number') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + (i == _self.info.length - 1 ? '' : ',\n')
+            } else if (data[i].type == 'string') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + (i == _self.info.length - 1 ? '"' : '",\n')
+            } else if (data[i].type == 'array[单个]') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + (i == _self.info.length - 1 ? ']' : '],\n')
+            } else if (data[i].type == 'array[多个]') {
+              let strArr = '';
+              for (let m = 0; m < yulanNum; m++) {
+                strArr += val + (m == yulanNum - 1 ? '' : ',')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == _self.info.length - 1 ? ']' : '],\n')
+            }
+          } else if (data[i].checked == '后面加') {
+            if (data[i].type == 'number') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + num + (i == _self.info.length - 1 ? '' : ',\n')
+            } else if (data[i].type == 'string') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + num + (i == _self.info.length - 1 ? '"' : '",\n')
+            } else if (data[i].type == 'array[单个]') {
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + num + (i == _self.info.length - 1 ? ']' : '],\n')
+            } else if (data[i].type == 'array[多个]') {
+              let strArr = '';
+              for (let m = 0; m < yulanNum; m++) {
+                strArr += '"' + val + m.toString() + (m == yulanNum - 1 ? '"' : '",')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == _self.info.length - 1 ? ']' : '],\n')
+            }
+          } else {
+            if (data[i].type == 'number') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":' + val + (i == _self.info.length - 1 ? '' : ',\n')
+            } else if (data[i].type == 'string') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":"' + val + (i == _self.info.length - 1 ? '"' : '",\n')
+            } else if (data[i].type == 'array[单个]') {
+              val = parseInt(val) + num;
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + val + (i == _self.info.length - 1 ? ']' : '],\n')
+            } else if (data[i].type == 'array[多个]') {
+              let strArr = '';
+              val = parseInt(val);
+              for (let m = 0; m < yulanNum; m++) {
+                strArr += '"' + (val + m) + (m == yulanNum - 1 ? '"' : '",')
+              }
+              str += (i == 0 ? '  {\n    "' : '    "') + key + '":[' + strArr + (i == _self.info.length - 1 ? ']' : '],\n')
+            }
+          }
+        }
+        str = str + (n == 2 - 1 ? '\r\n  }\n' : '\r\n  },\r\n');
+        num++;
+      }
+      str = "[\r\n" + str + "]";
+      _self.result = str;
+    },
+  },
+  mounted() {
+    let _self = this;
+    if (window.localStorage.getItem('info')) {
+      _self.info = JSON.parse(window.localStorage.getItem('info'));
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
